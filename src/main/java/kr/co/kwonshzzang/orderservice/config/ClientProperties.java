960a831c21a92b53381c62d12fae97a076f3a3f9
kwonshzzang <kwonshzzang@gmail.com>
2024-09-10 18:19:49 +0900

    Update ClientProperties

--- src/main/java/kr/co/kwonshzzang/orderservice/config/ClientProperties.java
+++ src/main/java/kr/co/kwonshzzang/orderservice/config/ClientProperties.java
@@ -6,7 +6,12 @@ import java.net.URI;
 
 @ConfigurationProperties(prefix = "polar")  //사용자 지정 속성 이름의 프리릭스
 public record ClientProperties(
+        String greeting,
         URI catalogServiceUri,   //카탈로그 서비스의 URI를 지정하는 속성. 널 값을 가질 수 없다.
-        String greeting
+        URI configServiceUri,     //컨피그 서비스의 URI를 지정하는 속성. 널 값을 가질 수 없다.
+        String flywayJdbcDatasourceUrl,
+        String r2dbcDatasourceUrl,
+        String r2dbcUsername,
+        String r2dbcPassword
 ) {
 }
